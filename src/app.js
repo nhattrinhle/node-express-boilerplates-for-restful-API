20b2cdfb7d1be71000df25812936cd1115dba617
Trinh Le <nhattrinh.le.it@gmail.com>
2023-12-22 16:21:14 +0700

    custom morgan used for logging http request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
-require('dotenv').config()
 const express = require('express')
+const config = require('./config/config')
+const morgan = require('./config/morgan')
 
 const app = express()
+
+if (config.env !== 'test') {
+    app.use(morgan.successHandler)
+    app.use(morgan.errorHandler)
+}
 
 module.exports = app
